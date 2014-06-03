c300ae4446c6214a2e7d53ddfdb000fcad3a2928
EclipseSpark <eclipse-github@frozenbox.org>
2014-06-03 21:32:18 +0200

    Update tccf_gpg_asymmetric_mod.sh

--- tccf_gpg_asymmetric_mod.sh
+++ tccf_gpg_asymmetric_mod.sh
@@ -1,15 +1,22 @@
 #! /bin/bash
 
-#Two Cents Crypto Frontend Copyright (C) 2014  Giovanni Santostefano
+#
+#Two Cents Crypto Frontend 0.1 - Copyright (C) 2014 Giovanni Santostefano
 #This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.
 #This is free software, and you are welcome to redistribute it
 #under certain conditions; type `show c' for details.
+#
+# Two Cents Crypto Frontend 0.2 - 2014 Software edited and modified by Lorenzo "EclipseSpark" Faletra <eclipse@frozenbox.org>
+
+
 
 #encryption strength choice
 dialog --clear --title "Asymmetric Encrypt Module" \
         --menu "Choose your activity" 15 60 6 \
-        "Keygen"  "Generate a key pair" \
+        "keyring" "Display your PUBLIC Keys Address Book" \
+        "Keyring" "Display your PRIVATE Keys available" /
         "AddKey"  "Add someone's public key to keyring" \
+        "Genkey"  "Generate a new key pair" \
         "Encrypt"  "Encrypt file with someone's public key" \
         "Decrypt" "Decrypt file with your private key" \
         "Sign" "Apply a digital signature to a file" \
@@ -21,7 +28,7 @@ rm /tmp/tccf.cryptdevice.temp.wakawaka
 
 case $retval in
   0)
-    if [[ $action =~ "Keygen" ]]
+    if [[ $action =~ "Genkey" ]]
     then
         clear
         bash gpg_asymmetric_keygen.sh
@@ -29,6 +36,14 @@ case $retval in
     then
         clear
         bash gpg_asymmetric_addpub.sh
+    elif [[ $action =~ "keyring" ]]
+    then
+        clear
+        bash gpg_asymmetric_keyring.sh
+    elif [[ $action =~ "Keyring" ]]
+    then
+        clear
+        bash gpg_asymmetric_Keyring.sh
     elif [[ $action =~ "Encrypt" ]]
     then
         clear
