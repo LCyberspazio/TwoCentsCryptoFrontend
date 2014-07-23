b834ecdcd6e9e409f2a243779f8a27bb594b0c03
LCyberspazio <idmgiovanni@libero.it>
2014-07-23 14:42:02 +0200

    Added directory tree encryption v0.3

--- tccf_utils/gpg_symmetric_dir_enc.sh
+++ tccf_utils/gpg_symmetric_dir_enc.sh
@@ -14,7 +14,7 @@ dialog --title "Directory Encryption" --clear \
        --msgbox "Encrypt of all files of a directory and subdirectory with a password.\n\nUseful to make DVDs with gpg encrypted backups usable on all gpg-ready systems, unlike LUKS filesystems" 20 50
        
 dialog --title "WARNING!!" --clear \
-       --msgbox "Do not use on multiuser systems\nPassword will be visible in top or ps programs.\n\nAll original files will be securely DELETED\n\nBugs may cause loss of data. If you don't know how to use it securely, please quit the program now! I'm not responsible of any damage... as always" 20 60
+       --msgbox "Do not use on multiuser systems\nPassword will be visible in top or ps programs.\n\nAll original files will be securely DELETED (srm -l lessen security)\n\nBugs may cause loss of data. If you don't know how to use it securely, please quit the program now! I'm not responsible of any damage... as always\n\nIt doesn't works on filenames with spaces!" 20 60
 
 
 if [ -e "/usr/bin/gpg" ]
@@ -69,7 +69,7 @@ do
     echo "Encrypting $file -> $file.asc"
     gpg -c --armor --passphrase $passphrase $file
     echo "Secure Deleting $file"
-    srm $file
+    srm -l $file
     echo "----------------------------------------"
 done
 
